5cf7f66a17cc019ad3ee6c6a62c304022070c8d8
Keegan Strohm <krstr96@gmail.com>
2017-10-21 12:29:43 -0400

    Implemented some voting methods.

--- app/src/main/java/com/BackendCode/Voting.java
+++ app/src/main/java/com/BackendCode/Voting.java
@@ -1,4 +1,5 @@
 package com.BackendCode;
+import java.util.ArrayList;
 
 /**
  * Created by keegan on 10/21/17.
@@ -6,20 +7,69 @@ package com.BackendCode;
 
 public class Voting {
 
-    public static void addItem(String item) {
+    private ArrayList<Data> items = new ArrayList<>();
 
-    };
+    public void addItem(String item) {
+        Data temp = new Data(item, 0);
+        items.add(temp);
+    }
 
-    public static void removeItem(String item) {
+    public void removeItem(String item) {
+        for (Data temp : this.items) {
+            if (temp.getName().equals(item)) {
+                this.items.remove(temp);
+            }
+        }
+    }
 
-    };
+    public int getVote(String item) {
+        int vote = 0;
+        for (Data temp : this.items) {
+            if (temp.restaurant.equals(item)) {
+                vote = temp.getVotes();
+            }
+        }
 
-    public static int getVoteCount(String item) {
+        return vote;
+    }
 
-        return 0;
-    };
+    public void addVote(String item) {
+        for (Data temp : this.items) {
+            if (temp.restaurant.equals(item)) {
+                temp.setVotes(temp.getVotes() + 1);
+            }
+        }
+    }
 
-    public static void addVoteToItem(String item) {
-        
-    };
+    public int size() {
+        return items.size();
+    }
+
+    private class Data {
+        private String restaurant;
+        private int votes;
+
+        public Data(String restaurant, int votes){
+            this.restaurant = restaurant;
+            this.votes = votes;
+        }
+
+        public void setName(String restaurant) {
+            this.restaurant = restaurant;
+        }
+
+        public void setVotes(int votes) {
+            this.votes = votes;
+        }
+
+        public String getName(){
+            return this.restaurant;
+        }
+
+        public int getVotes() {
+            return this.votes;
+        }
+
+    }
+
 }
