f9d7bca0d24802df15627a01e016a7deb61cf024
acongos92 <30706447+acongos92@users.noreply.github.com>
2017-10-21 12:05:17 -0400

    Incomplete recyclerview class

---
+++ app/src/main/java/Adapters/InputScreenRecyclerViewAdapter.java
@@ -0,0 +1,10 @@
+package Adapters;
+
+import android.support.v7.widget.RecyclerView;
+
+/**
+ * Created by andy on 10/21/2017.
+ */
+
+public class InputScreenRecyclerViewAdapter extends RecyclerView.Adapter<InputScreenRecyclerViewAdapter.ClassViewHolder> {
+}
