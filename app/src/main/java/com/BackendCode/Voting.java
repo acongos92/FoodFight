9617c1f05cd5a0e8ff770a50e4376ca694b492c2
Keegan Strohm <krstr96@gmail.com>
2017-10-21 12:01:36 -0400

    Added Voting Class

---
+++ app/src/main/java/com/BackendCode/Voting.java
@@ -0,0 +1,25 @@
+package com.BackendCode;
+
+/**
+ * Created by keegan on 10/21/17.
+ */
+
+public class Voting {
+
+    public static void addItem(String item) {
+
+    };
+
+    public static void removeItem(String item) {
+
+    };
+
+    public static int getVoteCount(String item) {
+
+        return 0;
+    };
+
+    public static void addVoteToItem(String item) {
+        
+    };
+}
